79115a5bd4fb61f2ca54a4940230937d49633006
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2024-01-18 20:41:34 +0530

    Update AppTest.java adding new testcase

--- src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
+++ src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
@@ -25,4 +25,19 @@ public class AppTest
         //String originalTitle = driver.getTitle();
         driver.close();
     }
+
+    @Test
+    public void TestNew2() throws InterruptedException { 
+    //System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("start-maximized"); // open Browser in maximized mode
+    options.addArguments("disable-infobars"); // disabling infobars
+    options.addArguments("--disable-extensions"); // disabling extensions
+    options.addArguments("--disable-gpu"); // applicable to windows os only
+    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+    options.addArguments("--no-sandbox"); // Bypass OS security model
+    driver = new ChromeDriver(options);
+    driver.get("https://toolsqa.com");
+    
+    }	
 }
