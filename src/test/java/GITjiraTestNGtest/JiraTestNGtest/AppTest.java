55ac9991fe273a2942040ef138d33631e8724d76
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2024-01-18 20:44:03 +0530

    Update AppTest.java

--- src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
+++ src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
@@ -2,8 +2,8 @@ package GITjiraTestNGtest.JiraTestNGtest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
