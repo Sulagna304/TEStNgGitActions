c7209f854626fa3e7d9c409ba59c297820e638d2
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2024-01-19 13:55:58 +0530

    Update AppTest.java

--- src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
+++ src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
@@ -14,20 +14,17 @@ public class AppTest
 	 // static WebDriver driver;
 	  
     
-    @Test
-    public void TestNew2() throws InterruptedException { 
-    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-	/*
-	 * ChromeOptions options = new ChromeOptions();
-	 * options.addArguments("start-maximized"); // open Browser in maximized mode
-	 * options.addArguments("disable-infobars"); // disabling infobars
-	 * options.addArguments("--disable-extensions"); // disabling extensions
-	 * options.addArguments("--disable-gpu"); // applicable to windows os only
-	 * options.addArguments("--disable-dev-shm-usage"); // overcome limited resource
-	 * problems options.addArguments("--no-sandbox"); // Bypass OS security model
-	 */  
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://toolsqa.com");
+   @Test
+    public void TestNew3() throws InterruptedException { 
+
+
+        String chromeBinaryPath = "chromedriver.exe";
+    	        ChromeOptions options = new ChromeOptions();
+    	        options.setBinary(chromeBinaryPath);
+    	        WebDriver driver = new ChromeDriver(options);
+    	        driver.get("https://toolsqa.com");
+               driver.quit();
+    	   	
     
-    }	
-}
+    }
+     }	
