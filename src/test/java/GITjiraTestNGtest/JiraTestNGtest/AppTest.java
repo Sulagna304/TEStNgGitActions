29f7b667070219a8c4d0b913bd8ad0ff34a57c31
Sulagna304 <132049925+Sulagna304@users.noreply.github.com>
2024-01-19 13:36:36 +0530

    Update AppTest.java

--- src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
+++ src/test/java/GITjiraTestNGtest/JiraTestNGtest/AppTest.java
@@ -16,15 +16,17 @@ public class AppTest
     
     @Test
     public void TestNew2() throws InterruptedException { 
-    //System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver.exe");
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("start-maximized"); // open Browser in maximized mode
-    options.addArguments("disable-infobars"); // disabling infobars
-    options.addArguments("--disable-extensions"); // disabling extensions
-    options.addArguments("--disable-gpu"); // applicable to windows os only
-    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-    options.addArguments("--no-sandbox"); // Bypass OS security model
-    driver = new ChromeDriver(options);
+    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	/*
+	 * ChromeOptions options = new ChromeOptions();
+	 * options.addArguments("start-maximized"); // open Browser in maximized mode
+	 * options.addArguments("disable-infobars"); // disabling infobars
+	 * options.addArguments("--disable-extensions"); // disabling extensions
+	 * options.addArguments("--disable-gpu"); // applicable to windows os only
+	 * options.addArguments("--disable-dev-shm-usage"); // overcome limited resource
+	 * problems options.addArguments("--no-sandbox"); // Bypass OS security model
+	 */  
+    WebDriver driver = new ChromeDriver();
     driver.get("https://toolsqa.com");
     
     }	
